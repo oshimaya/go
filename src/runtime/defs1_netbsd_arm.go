32d0ab9c1efd0d90d31ba2fec0c4cc2d0a8274fc
Yasushi Oshima <oshima-ya@yagoto-urayama.jp>
2016-02-05 04:19:12 +0900

    Add padding bytes to some structures using by arm EABI syscall

--- src/runtime/defs1_netbsd_arm.go
+++ src/runtime/defs1_netbsd_arm.go
@@ -110,6 +110,7 @@ type stackt struct {
 type timespec struct {
 	tv_sec  int64
 	tv_nsec int32
+	Pad_cgo_0 [4]byte
 }
 
 func (ts *timespec) set_sec(x int32) {
@@ -123,6 +124,7 @@ func (ts *timespec) set_nsec(x int32) {
 type timeval struct {
 	tv_sec  int64
 	tv_usec int32
+	Pad_cgo_0 [4]byte
 }
 
 func (tv *timeval) set_usec(x int32) {
@@ -136,9 +138,11 @@ type itimerval struct {
 
 type mcontextt struct {
 	__gregs [17]uint32
-	__fpu   [4 + 8*32 + 4]byte // EABI
+	Pad_cgo_0 [4]byte
+	__fpu   [272]byte
 	// __fpu [4+4*33+4]byte // not EABI
 	_mc_tlsbase uint32
+	Pad_cgo_1 [4]byte
 }
 
 type ucontextt struct {
@@ -146,6 +150,7 @@ type ucontextt struct {
 	uc_link     *ucontextt
 	uc_sigmask  sigset
 	uc_stack    stackt
+	Pad_cgo_0 [4]byte
 	uc_mcontext mcontextt
 	__uc_pad    [2]int32
 }
@@ -157,6 +162,7 @@ type keventt struct {
 	fflags uint32
 	data   int64
 	udata  *byte
+	Pad_cgo_0 [4]byte
 }
 
 // created by cgo -cdefs and then converted to Go
