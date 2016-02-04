854a32d2ecb0a011f121ec31102f0ae8096e8dbe
Yasushi Oshima <oshima-ya@yagoto-urayama.jp>
2016-02-05 04:14:31 +0900

    Change ELF_Phdr.p_align for NetBSD/{amd64,arm} from 4k to the same size as a system standard ld(GNU linker).
            amd64: 0x200000 (2MiB)
            arm: 0x10000 (64KiB)
    
    When p_align < PAGE_SIZE, NetBSD's ELF loader is not able to load a binary.
    And the default PAGE_SIZE of NetBSD/arm kernel for many machines which have
    armv6 or later CPU were changed from 4KiB to 8KiB since NetBSD 7.0.
    
    I aligned to the same value as the default value of NetBSD standard binary
    in amd64 too.
    
    BTW, the standard value in NetBSD/i386 is the same 4KiB.

--- src/cmd/link/internal/arm/obj.go
+++ src/cmd/link/internal/arm/obj.go
@@ -125,7 +125,6 @@ func archinit() {
 
 	case obj.Hlinux, /* arm elf */
 		obj.Hfreebsd,
-		obj.Hnetbsd,
 		obj.Hopenbsd:
 		ld.Debug['d'] = 0
 		// with dynamic linking
@@ -140,7 +139,20 @@ func archinit() {
 		if ld.INITRND == -1 {
 			ld.INITRND = 4096
 		}
-
+	case obj.Hnetbsd: /* NetBSD */
+		ld.Debug['d'] = 0
+		// with dynamic linking
+		ld.Elfinit()
+		ld.HEADR = ld.ELFRESERVE
+		if ld.INITTEXT == -1 {
+			ld.INITTEXT = 0x10000 + int64(ld.HEADR)
+		}
+		if ld.INITDAT == -1 {
+			ld.INITDAT = 0
+		}
+		if ld.INITRND == -1 {
+			ld.INITRND = 0x10000
+		}
 	case obj.Hnacl:
 		ld.Elfinit()
 		ld.HEADR = 0x10000
